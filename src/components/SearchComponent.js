a8bdd0a86c1295e4af061c33db11554a5087be5e
Aleja Ocampo <omaira.527@hotmail.com>
2022-04-17 15:43:29 -0500

    configurando estructura

---
+++ src/components/SearchComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchComponent = () => {
+  //setear los hooks useState
+
+  //funcion para traer los datos de la Api
+
+  //metodo de filtrado
+
+  //funcion de busqueda
+
+  //renderizamos la vista
+  return <div>SearchComponent</div>;
+};
+
+export default SearchComponent;
